a69d0aa05bf5043ff1b2ac90113158a15b71cf5b
Indranil Mukherjee <bhaikaju@gmail.com>
2020-11-05 00:41:05 +0530

    3. CSS & Modal Tinder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {LoginComponent} from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tinder-home';
+export class AppComponent implements OnInit {
+  showHeader1: boolean;
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    this.showHeader1 = false;
+  }
+
+  toggleHeader1(): void {
+    this.showHeader1 = !this.showHeader1;
+  }
+
+  login(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '500px',
+      height: '550px'
+    });
+
+  }
 }
